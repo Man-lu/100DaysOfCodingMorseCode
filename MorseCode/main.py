092c2946cca04da1b9ddea58165e21c7824b4313
ManluM <lucky.manaswe@gmail.com>
2021-01-05 15:20:12 +0200

    Final project

--- MorseCode/main.py
+++ MorseCode/main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+from colorama import Fore
+from data import morse_dict
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+continue_to_encode_or_decode = True
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(f"{Fore.MAGENTA}############## MORSE CODE | ENCODE AND DECODE MESSAGES ##############{Fore.MAGENTA}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def encode_message():
+    """"Encodes Normal Text, Numbers and Some Punctuation into Morse Code"""
+    message = input(f"****** TYPE IN THE MESSAGE TO ENCODE :\n{Fore.RESET}")
+    message = message.replace(" ", "|").upper()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    encoded_message = []
+    for char in message:
+        try:
+            encoded_message.append(morse_dict[char])
+        except KeyError:
+            print(f"{Fore.LIGHTRED_EX}******I Cannot Recognize One or More of Your Letters: ******{Fore.RESET}")
+        encoded_message.append(" ")
+
+    print(f"{Fore.MAGENTA}******Your Encoded Message is as follows: ******{Fore.RESET}")
+    print(f"{Fore.CYAN}<START> {Fore.RESET} {''.join(encoded_message)} {Fore.CYAN} <END>")
+
+
+def decode_message():
+    """"Decodes the Morse Code into a Normal Text"""
+    message = input(f"****** TYPE IN THE MORSE CODE TO DECODE "
+                    f"{Fore.RED}**USE 'SPACE' TO SEPARATE MORSE CODE REPRESENTING A LETTER AND '|' "
+                    f"TO SEPARATE WORDS :\n{Fore.RESET}\n").split(" ")
+
+    decoded_message = []
+    for char in message:
+        for key,value in morse_dict.items():
+            if value == char:
+                decoded_message.append(key)
+
+    decoded_message = "".join(decoded_message).replace("|", " ").title()
+
+    print(f"{Fore.MAGENTA}******Your Decoded Message is as follows: ******{Fore.RESET}")
+    print(f"{Fore.CYAN}<START> {Fore.RESET} {decoded_message} {Fore.CYAN} <END>")
+
+
+while continue_to_encode_or_decode:
+    encode_decode = input(f"{Fore.LIGHTGREEN_EX}############## Type 'E' to Encode and 'D' to Decode"
+                          f" and 'any key' to Quit ##############\n{Fore.RESET}").lower()
+    if encode_decode == 'e':
+        encode_message()
+    elif encode_decode == 'd':
+        decode_message()
+    else:
+        print(f"{Fore.RED}******You Have Quit Encoding or Decoding: ******{Fore.RESET}")
+        continue_to_encode_or_decode = False
